3b856c7862cc4654a7721064c8925f31cdd2d735
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-11-04 22:36:45 -0700

    .

--- QuickStart.java
+++ QuickStart.java
@@ -6,20 +6,12 @@ class QuickStart {
 		javax.swing.SwingUtilities.invokeLater(() -> {
 			//Create and set up the window.
 			JFrame frame = new JFrame("Test Window");
-			// add stop program on exit
+			frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 			
-
 			//Create blank label in content plane
 			JLabel emptyLabel = new JLabel("");
 			emptyLabel.setPreferredSize(new Dimension(1000, 600));
 			frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-			// JComponent contentPlaneBorder = new JComponent();
-
-			JPanel contentPlane = new JPanel(new BorderLayout());
-			// contentPlane.setBorder();
-
-			frame.setContentPane(contentPlane);
 
 			//Display the window.
 			frame.pack();
@@ -28,4 +20,4 @@ class QuickStart {
 	}
 }
 
-// https://docs.oracle.com/javase/tutorial/uiswing/components/jcomponent.html;
+// https://docs.oracle.com/javase/tutorial/uiswing/components/jcomponent.html
