90763d8fb4070d67dafd7f89a1c95adf398bbed5
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2021-11-11 12:55:14 -0800

    added random line generator from https://stackoverflow.com/questions/5801734/how-to-draw-lines-in-java

--- TestProject.java
+++ TestProject.java
@@ -1,10 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.LinkedList;
 
 class TestProject extends JPanel {
 
 	protected JButton stopButton, testPrintButton, lineButton;
+	private final LinkedList<Line> lines = new LinkedList<>();
+	static final TestProject comp = new TestProject();
 
 	class GenericButton extends JButton implements ActionListener {
 		public GenericButton(String buttonName) {
@@ -28,6 +31,12 @@ class TestProject extends JPanel {
 
 				case "Line":
 						System.out.println("Draw Line.");
+						int x1 = (int) (Math.random()*1000);
+						int x2 = (int) (Math.random()*1000);
+						int y1 = (int) (Math.random()*600);
+						int y2 = (int) (Math.random()*600);
+						Color randomColor = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+						comp.addLine(x1, y1, x2, y2, randomColor);
 					break;
 			
 				default:
@@ -35,6 +44,47 @@ class TestProject extends JPanel {
 			}
 		}		
 	}
+
+	private static class Line {
+		final int x1; 
+		final int y1;
+		final int x2;
+		final int y2;   
+		final Color color;
+	
+		public Line(int x1, int y1, int x2, int y2, Color color) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+			this.color = color;
+		}
+	}
+
+	public void addLine(int x1, int x2, int x3, int x4) {
+		addLine(x1, x2, x3, x4, Color.black);
+	}
+	
+	public void addLine(int x1, int x2, int x3, int x4, Color color) {
+		lines.add(new Line(x1,x2,x3,x4, color));        
+		repaint();
+	}
+
+	public void clearLines() {
+		lines.clear();
+		repaint();
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for (Line line : lines) {
+			g.setColor(line.color);
+			g.drawLine(line.x1, line.y1, line.x2, line.y2);
+		}
+	}
+
+
 
 	public TestProject() {
 		stopButton = new GenericButton("Stop");
@@ -50,17 +100,11 @@ class TestProject extends JPanel {
 		javax.swing.SwingUtilities.invokeLater(() -> {
 			//Create and set up the window.
 			JFrame frame = new JFrame("Test Window");
-			frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-			
-			TestProject newContentPane = new TestProject();
-			newContentPane.setOpaque(true);
-			frame.setContentPane(newContentPane);
-
+			frame.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
 			//Create blank label in content plane
-			JLabel emptyLabel = new JLabel("");
-			emptyLabel.setPreferredSize(new Dimension(1000, 600));
-			frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+			comp.setPreferredSize(new Dimension(1000, 600));
+			frame.getContentPane().add(comp, BorderLayout.CENTER);
 
 			//Display the window.
 			frame.pack();
